68b283f66207324db33dff6615a65d15fdbc6ad0
Claudio Camacho <claudio.camacho@fdx-mac.lan>
2022-03-07 15:41:48 +0200

    Add missing files for move-generation (and checking square attacks).

---
+++ src/movgen.h
@@ -0,0 +1,73 @@
+/*
+  This file is part of Gargantua, a UCI chess engine with NNUE evaluation
+  derived from Chess0, and inspired by Code Monkey King's bbc-1.4.
+     
+  Copyright (C) 2022 Claudio M. Camacho
+ 
+  Gargantua is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+ 
+  Gargantua is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MOVGEN_H
+#define MOVGEN_H
+
+#include "bitboard.h"
+#include "position.h"
+
+
+
+// isSquareAttacked
+//
+// True if the given square is attacked by any piece an opponent's piece.
+static inline bool isSquareAttacked(int square, int side)
+{
+    // square attacked by White or Black pawns
+    if ((side == White) && (PawnAttacks[Black][square] & bitboards[P]))
+        return true;
+    
+    if ((side == Black) && (PawnAttacks[White][square] & bitboards[p]))
+        return true;
+   
+
+    // square attacked by Knights
+    if (KnightAttacks[square] & ((side == White) ? bitboards[N] : bitboards[n]))
+        return true;
+
+    
+    // square attacked by Bishops
+    if (getBishopAttacks(square, occupancies[Both]) & ((side == White) ? bitboards[B] : bitboards[b]))
+        return true;
+
+
+    // square attacked by Rooks
+    if (getRookAttacks(square, occupancies[Both]) & ((side == White) ? bitboards[R] : bitboards[r]))
+        return true;
+
+
+    // square attacked by Queens
+    if (getQueenAttacks(square, occupancies[Both]) & ((side == White) ? bitboards[Q] : bitboards[q]))
+        return true;
+
+    
+    // square attacked by Kings
+    if (KingAttacks[square] & ((side == White) ? bitboards[K] : bitboards[k]))
+        return true;
+
+
+    // by default return false
+    return false;
+}
+
+
+
+#endif  //  MOVGEN_H
