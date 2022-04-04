b562cc4497ba4f6806dbaec36b996ade8956f793
Claudio Camacho <claudio.camacho@fdx-mac.lan>
2022-04-04 21:24:51 +0300

    Add TTEntry_t and transposition tables data structures.
    We use a C-stye array due to a much faster speed in read and
    write performance. The array contains TTEntry objects, which
    take 64 bits of space each (8 bytes). That means a cache size
    of 128MB will contain about 16M entries.

--- src/tt.h
+++ src/tt.h
@@ -35,12 +35,62 @@ extern Bitboard side_key;
 
 
 
+// Transposition Table implementation:
+//
+// We use a C-stye array due to a much faster speed in read and
+// write performance. The array contains TTEntry objects, which
+// take 64 bits of space each (8 bytes).
+//
+// That means a cache size of 128MB will contain about 16M entries.
+
+
+// initial hash size (~128MB)
+extern uint32_t hash_size;
+
+
+// number hash table entries
+extern uint64_t hash_entries;
+
+
+// no hash entry found constant
+#define no_hash_entry 100000
+
+
+// transposition table hash flags (node type)
+#define hash_type_exact 0
+#define hash_type_alpha 1
+#define hash_type_beta  2
+
+
+// TTEntry struct is the 8 bytes transposition table entry, defined as below:
+//
+// key        16 bit
+// depth       8 bit
+// type        8 bit
+// value      32 bit
+// best_move  16 bit
+//
+// Total size (per entry): 64 bits / 8 bytes
+typedef struct {
+    uint64_t key;   
+    uint8_t  depth;      
+    uint8_t  type;       
+    int      value;
+    int      best_move;
+} TTEntry_t;
+
+extern TTEntry_t *hash_table;
+
+
+
 // initRandomKeys
 //
 // Functionality to initialize, generate, access and manipulate Zobrist
 // hash keys as well as transposition tables.
 void initRandomKeys();
 uint64_t generateHashkey();
+void clear_hash_table();
+void init_hash_table(uint32_t);
 
 
 
